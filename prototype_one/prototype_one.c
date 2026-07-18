c77064a00210a200af30abdf16c7826440ee2e5d
RATSMORE <ratsmore@gmail.com>
2026-07-17 19:28:13 -0500

    LED now works based on EXTI
    proper implementation of EXTI (I was really just missing a lot of reserved addresses in the startup's vector table)
    
    Im going to cry now

--- prototype_one/prototype_one.c
+++ prototype_one/prototype_one.c
@@ -3,7 +3,7 @@
     a push-button. To implement this, a NUCLEO-F411RE board's user LED and push-button will be used.
 */
 
-
+#include <stdint.h>
 /* definitions related to the LED pin*/
 #define RCC_BASE            (0x40023800U)
 #define RCC_AHB1ENR         *((volatile unsigned int*) (RCC_BASE + 0x30U))
@@ -33,11 +33,14 @@
 #define EXTI13_IQR_POS      (8U)
 #define SYSCFG_BASE         (0x40013800U)
 #define SYSCFG_EXTICR4      *((volatile unsigned int*) (SYSCFG_BASE + 0x14U))
-#define BUTTON_PIN_CR4      (4U)
+#define BUTTON_PIN_CR4      (0b10U << 4U)
 #define RCC_APB2ENR         *((volatile unsigned int*) (RCC_BASE + 0x44U))
 #define APB2ENR_SYSCFG      (1U << 14U)
 #define EXTI_PR             *((volatile unsigned int*) (EXTI_BASE + 0x14U))
 
+
+
+#define GPIOC_IDR           *((volatile unsigned int*) (GPIOC_BASE + 0x10))
 
 void configure_LED_pin(void);
 void configure_button_pin(void);
@@ -46,13 +49,20 @@ void EXTI15_10_Handler(void);
 
 int main()
 {
-    RCC_APB2ENR |= APB2ENR_SYSCFG; // enable syscfg clock
     configure_LED_pin();
     configure_button_pin();
-    
     configure_button_interrupt();
-    GPIOA_ODR |= (1<<5); // temporary test. If this turns on then the stuff beforehand worked (in theory)
-    while(1);
+    
+   /* while(1)
+    {
+        if (GPIOC_IDR & (1U << 13U)) //check if the button is not pressed
+        {
+            GPIOA_ODR |= (1U << 5U); //turn on the led
+        } else {
+            GPIOA_ODR &= ~(1U << 5U);
+        }
+    }*/
+   while(1);
 }
 
 
@@ -66,30 +76,32 @@ void configure_LED_pin(void) // Just set PA5 to output mode
 void configure_button_pin(void) // Set PC13 to input and pulldown
 {
     RCC_AHB1ENR |= GPIOCEN;
-    GPIOC_MODER &= ~(0b11 << BUTTON_PIN_MODER);
-    GPIOC_PUPDR &= ~(0b11 << BUTTON_PIN_PUPDR); // Clearing before setting input type
+    GPIOC_MODER &= ~(0b11 << BUTTON_PIN_MODER); // Clearing before setting input type
+    GPIOC_PUPDR &= ~(0b11 << BUTTON_PIN_PUPDR);
     GPIOC_PUPDR |= (0b01 << BUTTON_PIN_PUPDR); // must be 01 on nucleo because it has external wiring to pullup
 }
 
 void configure_button_interrupt(void) /* Set it so pushing the button results in an interrupt */
 {
+    RCC_APB2ENR |= APB2ENR_SYSCFG; // enable syscfg clock
     //SYSCFG_EXTICR4 &= ~(0b1111U << BUTTON_PIN_CR4);
-    SYSCFG_EXTICR4 |= (0b0010U << BUTTON_PIN_CR4); // Set it for port C pin 13
-
-    EXTI_IMR |= BUTTON_INT_IMR; // Unmask EXTI13 interrupt
-    EXTI_FTSR |= BUTTON_INT_FTSR; // Set it to falling edge interrupt
+    SYSCFG_EXTICR4 |= BUTTON_PIN_CR4; // Set it for port C pin 13
 
     
-    EXTI_PR = (1U << 13U);       // Clear pending flag by writing a 1 back to the register
-    NVIC_ISER1 = (1U << EXTI13_IQR_POS); // interrupt enable
+    EXTI_FTSR |= BUTTON_INT_FTSR; // Set it to falling edge interrupt
+    EXTI_IMR |= BUTTON_INT_IMR; // Unmask EXTI13 interrupt
+    NVIC_ISER1 = (1U << 8U); // interrupt enable
+    //EXTI_PR = (1U << 13U);       // Clear pending flag by writing a 1 back to the register
 
 }
 
 void EXTI15_10_Handler(void)
 {
+    
     if (EXTI_PR & (1U << 13U)) 
     {
-        GPIOA_ODR ^= LED_PIN_ODR;    // Toggle PA5 User LED
         EXTI_PR = (1U << 13U);       // Clear pending flag by writing a 1 back to the register
+        GPIOA_ODR ^= LED_PIN_ODR;    // Toggle PA5 User LED
+        
     }
 }
